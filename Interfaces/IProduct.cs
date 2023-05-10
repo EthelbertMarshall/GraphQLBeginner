87e62979c6c1c3ba4dd26d6f180ae78538f7f82e
EthelbertMarshall <remmarin@gmail.com>
2023-05-11 01:12:57 +0530

    Add project files.

---
+++ Interfaces/IProduct.cs
@@ -0,0 +1,14 @@
+﻿using GraphQLBeginner.Models;
+
+namespace GraphQLBeginner.Interfaces
+{
+    public interface IProduct
+    {
+        List<Product> GetAllProducts();
+        Product GetProductById(int id);
+        Product AddProduct(Product product);
+        Product UpdateProduct(int id,Product product);
+        void DeleteProduct(int id);
+
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,45 @@
+﻿using GraphQLBeginner.Interfaces;
+using GraphQLBeginner.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraphQLBeginner.Services
+{
+     
+    public class ProductService : IProduct 
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product() { Id = 0, Name="Coffee",  Price=10},
+            new Product() { Id = 1, Name="Tea",  Price=20},
+            new Product() { Id = 2, Name="Cold Drinks",  Price=30}
+        };
+        
+        public Product AddProduct(Product product)
+        {
+            products.Add(product);
+            return product;
+        }
+
+        public void DeleteProduct(int id)
+        {
+            products.RemoveAt(id);
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return products.ToList();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return products.Find(x => x.Id == id);
+            //return (products[id] as Product);
+        }
+
+        public Product UpdateProduct(int id, Product product)
+        {
+            products[id] = product;
+            return product;
+        }
+    }
+}
