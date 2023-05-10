4030b391fbbfd2f2c7aa6072341277f915f752de
EthelbertMarshall <remmarin@gmail.com>
2023-05-11 01:18:51 +0530

    Created Product Type Class

---
+++ Type/ProductType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using GraphQLBeginner.Models;
+
+namespace GraphQLBeginner.Type
+{
+    public class ProductType : ObjectGraphType<Product>
+    {
+        public ProductType()
+        {
+            Field(p => p.Id);
+            Field(p => p.Name);
+            Field(p => p.Price);
+        }
+    }
+}
